2ceb6f18e17b3165e11e6d806e8a2e2904919005
Nora <75159987+Eleonora474@users.noreply.github.com>
2021-05-31 22:24:15 +0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import { List } from 'semantic-ui-react'
+import { Email, Vk, Phone, Telegram, Viber, Whatsapp } from './Icons'
 
 const Footer = () => {
   return (
-    <List horizontal style={{ textAlign: 'center' }}>
-      <List.Item href='#'>ВК</List.Item>
-      <List.Item href='#'>ОК</List.Item>
-      <List.Item href='#'>ИН</List.Item>
-      <List.Item href='#'>ФБ</List.Item>
-      <List.Item href='#'>ЧТонибудь</List.Item>
+    <List horizontal style={{ textAlign: 'center', marginTop: '1rem' }}>
+      <List.Item href='#'>
+        <Whatsapp />
+      </List.Item>
+      <List.Item href='#'>
+        <Email />
+      </List.Item>
+      <List.Item href='#'>
+        <Vk />
+      </List.Item>
+
+      <List.Item href='#'>
+        <Telegram />
+      </List.Item>
+      <List.Item href='#'>
+        <Viber />
+      </List.Item>
+      <List.Item href='#'>
+        <Phone />
+      </List.Item>
     </List>
   )
 }
